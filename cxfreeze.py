349206b4d9eb038782058ddfcd24a1837c5593ab
Tabeth Nkangoh <tabeth@tabeth.com>
2018-05-29 14:08:51 -0400

    Windows builds now possible | Resolves #2

--- cxfreeze.py
+++ cxfreeze.py
@@ -1,16 +1,35 @@
 from cx_Freeze import setup, Executable
+import sys
+
+base = None
+
 
 executables = [
-    Executable('xorigin/xorigin.py')
+    Executable(
+        script='xorigin/xorigin.py',
+        base=base,
+        icon=None
+    )
 ]
 
-setup(name="xorigin",
+build_exe_options = {
+        "packages": ["os", "yaml", "twisted.internet", "zope.interface", "pkg_resources._vendor", "idna"],
+        "excludes": [],
+        "includes": []
+}
+
+setup(
+      name="xorigin.exe",
       version="0.1",
       description="Easy to use, portable CORS reverse proxy",
+      url="https://github.com/experianplc/xorigin",
       executables=executables,
       author="Tabeth Nkangoh | Experian",
       author_email="tabeth.nkangoh@experian.com",
       license="MIT",
-      keywords=["Cross origin", "Reverse proxy", "CORS Server", "Server"])
+      packages=["xorigin"],
+      options={"build_exe": build_exe_options},
+      keywords=["Cross origin", "Reverse proxy", "CORS Server", "Server"],
+)
 
 
